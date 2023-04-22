535b3ed50da8b6f5ca9d40e99196dfca06a32fa5
Erkin Kubatov <qoobatov@gmail.com>
2023-04-22 19:16:07 +0600

    test web app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Ermek, введи логин:</h1>
+      <h1>Введите логин и пароль:</h1>
       <div>
         <form action="">
           <input type="text" />
