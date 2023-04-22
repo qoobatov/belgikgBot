0cc8dd83f22f98b873eb8219214d333e1e5cb52a
Erkin Kubatov <qoobatov@gmail.com>
2023-04-22 16:35:48 +0600

    test changes in page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Privet</h1>
+      <form action="">
+        <label>
+          Введите свой логин:
+          <input type="text" />
+        </label>
+      </form>
     </div>
   );
 }
