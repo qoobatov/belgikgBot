0aeb9c22a4dd80991a5886db7b02442b2653551b
Erkin Kubatov <qoobatov@gmail.com>
2023-04-23 17:30:45 +0600

    fixed routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import "./App.css";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login/Login";
-// import MyTrades from "./components/MyTrades/MyTrades";
+import MyTrades from "./components/MyTrades/MyTrades";
 
 function App() {
   return (
     <>
       <div className="app">
-        <Login />
-        {/* <Router>
+        <Router>
           <Routes>
             <Route path="/belgikgBot" element={<Login />} />
             <Route path="/belgikgBot/mytrades" element={<MyTrades />} />
           </Routes>
-        </Router> */}
+        </Router>
       </div>
     </>
   );
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,14 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Login.css";
 import { Button, Form, Input } from "antd";
-// import { useNavigate } from "react-router-dom";
-import  clients  from "../../clients.json";
-import MyTrades from "../MyTrades/MyTrades";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import clients from "../../clients.json";
 
 function Login() {
-//   const navigate = useNavigate();
+  const navigate = useNavigate();
   const [error, setError] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const onFinish = (values) => {
     const { username, password } = values;
@@ -16,10 +15,11 @@ function Login() {
       (user) => user.username === username && user.password === password
     );
     if (employee) {
-      setIsLoggedIn(true);
+      navigate("/belgikgBot/mytrades");
+      // Перенаправляем на другую страницу
     } else {
       setError(true);
-      console.log("Нет такого пользователя");
+      console.log("Нету такого пользователя");
     }
   };
 
@@ -29,158 +29,59 @@ function Login() {
 
   return (
     <div className="login-container">
-      {isLoggedIn ? (
-        <MyTrades />
-      ) : (
-        <>
-          <h3>Введите логин и пароль:</h3>
-          <Form
-            name="basic"
-            initialValues={{
-              remember: true,
-            }}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-            autoComplete="off"
-          >
-            {error && (
-              <span
-                style={{ color: "red", margin: "0 auto", fontSize: "17px" }}
-              >
-                Неверный логин или пароль:
-              </span>
-            )}
-            <Form.Item
-              name="username"
-              placeholder="Username"
-              rules={[
-                {
-                  required: true,
-                  message:
-                    "Пожалуйста, введите имя пользователя или почту",
-                },
-              ]}
-            >
-              <Input placeholder="Login" />
-            </Form.Item>
+      <h3>Введите логин и пароль:</h3>
+      <Form
+        name="basic"
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        {error && (
+          <span style={{ color: "red", margin: "0 auto", fontSize: "17px" }}>
+            Неверный логин или пароль:
+          </span>
+        )}
+        <Form.Item
+          name="username"
+          placeholder="Username"
+          rules={[
+            {
+              required: true,
+              message: "Пожалуйста, введите имя пользователя или почту",
+            },
+          ]}
+        >
+          <Input placeholder="Login" />
+        </Form.Item>
 
-            <Form.Item
-              name="password"
-              rules={[
-                {
-                  required: true,
-                  message: "Пожалуйста, введите пароль",
-                },
-              ]}
-            >
-              <Input.Password placeholder="Пароль" />
-            </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Пожалуйста, введите пароль",
+            },
+          ]}
+        >
+          <Input.Password placeholder="Пароль" />
+        </Form.Item>
 
-            <Form.Item>
-              <div className="btn-and-forgot-pass-container">
-                <Button type="primary" htmlType="submit">
-                  Войти
-                </Button>
-                {/* <a className="login-form-forgot" href="">
-                Забыли пароль?
-            </a> */}
-              </div>
-            </Form.Item>
-          </Form>
-        </>
-      )}
+        <Form.Item>
+          <div className="btn-and-forgot-pass-container">
+            <Button type="primary" htmlType="submit">
+              Войти
+            </Button>
+            {/* <a className="login-form-forgot" href="">
+            Забыли пароль?
+        </a> */}
+          </div>
+        </Form.Item>
+      </Form>
     </div>
   );
 }
 
 export default Login;
-
-
-
-// import React from "react";
-// import "./Login.css";
-// import { Button, Form, Input } from "antd";
-// import { useNavigate } from "react-router-dom";
-// import { useState } from "react";
-// import clients from "../../clients.json";
-
-// function Login() {
-//   const navigate = useNavigate();
-//   const [error, setError] = useState(false);
-
-//   const onFinish = (values) => {
-//     const { username, password } = values;
-//     const employee = clients.users.find(
-//       (user) => user.username === username && user.password === password
-//     );
-//     if (employee) {
-//       navigate("/belgikgBot/mytrades");
-//       // Перенаправляем на другую страницу
-//     } else {
-//       setError(true);
-//       console.log("Нету такого пользователя");
-//     }
-//   };
-
-//   const onFinishFailed = (errorInfo) => {
-//     console.log("Failed:", errorInfo);
-//   };
-
-//   return (
-//     <div className="login-container">
-//         <h3>Введите логин и пароль:</h3>
-//       <Form
-//         name="basic"
-//         initialValues={{
-//           remember: true,
-//         }}
-//         onFinish={onFinish}
-//         onFinishFailed={onFinishFailed}
-//         autoComplete="off"
-//       >
-//         {error && (
-//           <span style={{ color: "red", margin: "0 auto", fontSize: "17px" }}>
-//             Неверный логин или пароль:
-//           </span>
-//         )}
-//         <Form.Item
-//           name="username"
-//           placeholder="Username"
-//           rules={[
-//             {
-//               required: true,
-//               message: "Пожалуйста, введите имя пользователя или почту",
-//             },
-//           ]}
-//         >
-//           <Input placeholder="Login" />
-//         </Form.Item>
-
-//         <Form.Item
-//           name="password"
-//           rules={[
-//             {
-//               required: true,
-//               message: "Пожалуйста, введите пароль",
-//             },
-//           ]}
-//         >
-//           <Input.Password placeholder="Пароль" />
-//         </Form.Item>
-
-//         <Form.Item>
-//           <div className="btn-and-forgot-pass-container">
-//             <Button type="primary" htmlType="submit">
-//               Войти
-//             </Button>
-//             {/* <a className="login-form-forgot" href="">
-//             Забыли пароль?
-//         </a> */}
-//           </div>
-//         </Form.Item>
-//       </Form>
-//     </div>
-//   );
-// }
-
-// export default Login;
