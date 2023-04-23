34d2a2ad00b389dbb7e17165def0dc37833bd017
Erkin Kubatov <qoobatov@gmail.com>
2023-04-23 09:48:40 +0600

    added resize form login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -28,58 +28,58 @@ function Login() {
   };
 
   return (
-    <Form
-      name="basic"
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      autoComplete="off"
-    >
-      {error && (
-        <span style={{ color: "red", margin: '0 auto', fontSize: '17px' }}>Неверный логин или пароль:</span>
-      )}
-      <Form.Item
-        name="username"
-        placeholder="Username"
-        rules={[
-          {
-            required: true,
-            message: "Пожалуйста, введите имя пользователя или почту",
-          },
-        ]}
+    <>
+      <Form
+        name="basic"
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
       >
-        <Input placeholder="LOGIN" />
-      </Form.Item>
+        {error && (
+          <span style={{ color: "red", margin: "0 auto", fontSize: "17px" }}>
+            Неверный логин или пароль:
+          </span>
+        )}
+        <Form.Item
+          name="username"
+          placeholder="Username"
+          rules={[
+            {
+              required: true,
+              message: "Пожалуйста, введите имя пользователя или почту",
+            },
+          ]}
+        >
+          <Input placeholder="Login" />
+        </Form.Item>
 
-      <Form.Item
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: "Пожалуйста, введите пароль",
-          },
-        ]}
-      >
-        <Input.Password placeholder="ПАРОЛЬ" />
-      </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Пожалуйста, введите пароль",
+            },
+          ]}
+        >
+          <Input.Password placeholder="Пароль" />
+        </Form.Item>
 
-      <Form.Item name="remember" valuePropName="checked">
-        <Checkbox>Запомнить меня</Checkbox>
-      </Form.Item>
-
-      <Form.Item>
-        <div className="btn-and-forgot-pass-container">
-          <Button type="primary" htmlType="submit">
-            ВОЙТИ
-          </Button>
-          {/* <a className="login-form-forgot" href="">
+        <Form.Item>
+          <div className="btn-and-forgot-pass-container">
+            <Button type="primary" htmlType="submit">
+              Войти
+            </Button>
+            {/* <a className="login-form-forgot" href="">
             Забыли пароль?
-          </a> */}
-        </div>
-      </Form.Item>
-    </Form>
+        </a> */}
+          </div>
+        </Form.Item>
+      </Form>
+    </>
   );
 }
 
