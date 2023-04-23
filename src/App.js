3751d4f5cd98b52a69bc6fec7bc8fafc90f22a4f
Erkin Kubatov <qoobatov@gmail.com>
2023-04-23 13:14:52 +0600

    fix routes

--- src/App.js
+++ src/App.js
@@ -3,16 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login/Login";
 import MyTrades from "./components/MyTrades/MyTrades";
 
-
-
-
 function App() {
   return (
     <>
       <div className="app">
         <Router>
           <Routes>
-            <Route path="/belgikgBot" element={<Login />} />
+            <Route path="/" element={<Login />} />
             <Route path="/belgikgBot/mytrades" element={<MyTrades />} />
           </Routes>
         </Router>
