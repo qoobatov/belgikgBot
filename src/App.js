c94d816575e62c694598ee4ca72d58eb34f991a7
Erkin Kubatov <qoobatov@gmail.com>
2023-04-23 21:16:40 +0600

    actual 9/16

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <Router>
           <Routes>
             <Route path="/belgikgBot" element={<Login />} />
-            <Route path="/mytrades" element={<MyTrades />} />
+            <Route path="/belgikgBot/mytrades" element={<MyTrades />} />
           </Routes>
         </Router>
       </div>
