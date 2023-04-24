59bfad076046828ac1cb4b7dc6e91ab631e0af10
Erkin Kubatov <qoobatov@gmail.com>
2023-04-24 10:05:18 +0600

    actual 10/05

--- src/App.js
+++ src/App.js
@@ -1,44 +1,21 @@
-// import "./App.css";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Login from "./components/Login/Login";
-// import MyTrades from "./components/MyTrades/MyTrades";
-
-// function App() {
-//   return (
-//     <>
-//       <div className="app">
-//         <Router>
-//           <Routes>
-//             <Route path="/belgikgBot" element={<Login />} />
-//             <Route path="/belgikgBot/mytrades" element={<MyTrades />} />
-//           </Routes>
-//         </Router>
-//       </div>
-//     </>
-//   );
-// }
-
-// export default App;
-
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import {Routes, Route } from "react-router-dom";
 import Login from "./components/Login/Login";
 import MyTrades from "./components/MyTrades/MyTrades";
 
 function App() {
-return (
-<>
-<div className="app">
-<Router basename={process.env.PUBLIC_URL}>
-<Routes>
-<Route path="/belgikgBot" element={<Login />} />
-<Route path="/belgikgBot/mytrades" element={<MyTrades />} />
-<Route path="/*" element={<Navigate to="/belgikgBot" />} />
-</Routes>
-</Router>
-</div>
-</>
-);
+  return (
+    <>
+      <div className="app">
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/mytrades" element={<MyTrades />} />
+          </Routes>
+      </div>
+    </>
+  );
 }
 
 export default App;
+
+
