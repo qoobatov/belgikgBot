05fb1d091a7aac638dcfdda742b82c5ae831b8d7
Erkin Kubatov <qoobatov@gmail.com>
2023-04-25 15:09:09 +0600

    edited bayer component

--- src/components/BayerServices/BayerServices.js
+++ src/components/BayerServices/BayerServices.js
@@ -3,17 +3,10 @@ import "./BayerServices.css";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { PlusOutlined } from "@ant-design/icons";
-import {
-  Button,
-  DatePicker,
-  Form,
-  Input,
-  InputNumber,
-  Select,
-  Upload,
-} from "antd";
+import { Checkbox } from "antd";
 
-const { RangePicker } = DatePicker;
+import { Button, DatePicker, Form, Input, Select, Upload } from "antd";
+
 const { TextArea } = Input;
 const normFile = (e) => {
   if (Array.isArray(e)) {
@@ -30,6 +23,18 @@ function BayerServices() {
     setshowNewOrder(true);
     navigate("/neworder");
   };
+  const { Option } = Select;
+  const CheckboxGroup = Checkbox.Group;
+
+  const options = [
+    { label: "Женский", value: "female" },
+    { label: "Мужской", value: "male" },
+    { label: "Детский", value: "kids" },
+  ];
+
+  function handleChangeType(checkedValues) {
+    console.log("checked = ", checkedValues);
+  }
 
   return (
     <>
@@ -49,31 +54,51 @@ function BayerServices() {
             }}
           >
             <Form.Item label="Тип товара">
-              <Select>
+              <CheckboxGroup options={options} onChange={handleChangeType} />
+
+              {/* <Select>
                 <Select.Option value="female">Женский</Select.Option>
                 <Select.Option value="male">Мужской</Select.Option>
                 <Select.Option value="kids">Детский</Select.Option>
                 <Select.Option value="kids">Все</Select.Option>
-              </Select>
-              <Form.Item label="Описание товара">
+              </Select> */}
+              <Form.Item label="Описание товара:">
                 <TextArea rows={3} />
               </Form.Item>
-              <Form.Item label="Input">
-                <Input />
+
+              <Form.Item
+                label="Цена за шт.  (выбрать валюту):"
+                style={{ display: "flex" }}
+              >
+                <Input type="number" style={{ width: "250px" }} />
+                <Select defaultValue="KGZ" style={{ width: 80 }}>
+                  <Option value="GBP">KGZ</Option>
+                  <Option value="USD">USD</Option>
+                  <Option value="EUR">EUR</Option>
+                  <Option value="RUB">RUB</Option>
+                  <Option value="KZT">KZT</Option>
+                </Select>
+              </Form.Item>
+
+              <Form.Item label="Количество:">
+                <Input type="number" />
               </Form.Item>
             </Form.Item>
-            <Form.Item label="DatePicker">
-              <DatePicker />
+            <Form.Item label="Дата:">
+              <DatePicker style={{ width: "330px" }} />
+            </Form.Item>
+            <Form.Item label="Адрес доставки:">
+              <Input />
+            </Form.Item>
+            <Form.Item label="Город доставки:">
+              <Input />
             </Form.Item>
             <Form.Item
               label="Прикрепить фото"
               valuePropName="fileList"
               getValueFromEvent={normFile}
             >
-              <Upload
-                action="/upload.do"
-                listType="picture-card"
-              >
+              <Upload action="/upload.do" listType="picture-card" maxCount={4}>
                 <div>
                   <PlusOutlined />
                   <div
